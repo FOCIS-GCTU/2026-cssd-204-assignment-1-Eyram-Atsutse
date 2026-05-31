0eb4a695ae3064de13caaccc3560760327117dd1
Eyram Atsutse <atsutsehayfordeyram@gmail.com>
2026-05-31 19:16:25 +0200

    Add function to print initials AEK
    Implemented a function to print my initials in a stylized format.

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+#File:initials.py
+#Description:Print out my initials
+#Assignment Number: 2
+#
+#Name:Atsutse Eyram Kojo
+#STUDENT ID:2425400628
+#Email: 2425400628@live.gctu.edu.gh
+#Grader : Augustus Buckman
+#
+#On my honor , Atsutse Eyram Kojo , this programming assignment is my own work
+#and I have not provided this to any other student.
+
 
 def main():
-  pass #code goes here
-
+    #printing my initials
+    print( )
+    print("...AEK")
+    print( )
+    print(".......AAA.............EEEEEEEEEEEE........KKKK.....KKK ...")
+    print("......AAAAA............EEEEEEEEEEEE........KKKK...KKKK....")
+    print(".....AAAAAAAA..........EEEE................KKKK..KKKK.....")
+    print("....AAA...AAAA.........EEEE................KKKKKKKK.......")
+    print("...AAAAAAAAAAAA........EEEEEEEEEE..........KKKKKK.........")
+    print("...AAAAAAAAAAAA........EEEE................KKKKKKKK.......")
+    print("...AAAA....AAAA........EEEE................KKKK..KKKK.....")
+    print("...AAAA....AAAA........EEEE................KKKK....KKKK...")
+    print("...AAAA....AAAA...**...EEEEEEEEEEEE...**...KKKK....KKKK...**")
+    print("...AAAA....AAAA...**...EEEEEEEEEEEE...**...KKKK....KKKK...**")
 
 
 main()
